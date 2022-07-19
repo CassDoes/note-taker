177bebb7c58538d2a51aa84b3dfe6482b19d9908
CassDoes <c.doexo@gmail.com>
2022-07-19 16:00:27 -0500

    fixed post request

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -11,24 +11,23 @@ router.get('/notes', (req, res) => {
 
 // Post request to add a new note 
 router.post('/notes', (req, res) => {
-  let newNote = req.body;
-  // give new note an unique id
-  newNote.id = uuidv4();
+    let newNote = req.body;
+    // give new note an unique id
+    newNote.id = uuidv4();
 
-  // push new note to notes array
-  notes.push(newNote);
+    // push new note to notes array
+    notes.push(newNote);
 
-      // write file to show updated note column
-      fs.writeFile('db/db.json', JSON.stringify(notes), err => {
-          if (err) {
-              return console.log(err);
-          }
-          console.log('Note Saved!')
-          
-      })
-      return res.json(newNote);
+        // write file to show updated note column
+        fs.writeFile('db/db.json', JSON.stringify(notes), err => {
+            if (err) {
+                return console.log(err);
+            }
+            console.log('Note Saved!')
+            
+        })
+        return res.json(newNote);
 })
-
 
 // Delete request to remove saved note
 router.delete('/notes/:id', (req, res) => {
